75275cc079903475e9a549f9349a843644dfead9
Morton Fox <github@qslw.com>
2023-06-02 20:21:21 -0400

    Update coupon clicker

--- main.js
+++ main.js
@@ -4,33 +4,47 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function findButtons(str) {
+  let btns = [];
+  for (let btn of document.querySelectorAll('button')) {
+    if (btn.textContent.includes(str)) {
+      btns.push(btn);
+    }
+  }
+  return btns;
+}
+
 async function runSelect(event) {
   event.preventDefault();
 
-  // Scroll to the bottom a few times to try to get all coupons to show up.
-  for (let i = 0; i < 5; ++i) {
-    window.scrollTo(0, document.body.scrollHeight);
+  // Click on "Show More" button until it no longer shows up.
+  for (;;) {
+    let btns = findButtons('Show More');
+    if (!btns.length) break;
+    let btn = btns[0];
+    btn.scrollIntoView();
+    btn.click();
     await sleep(1000);
   }
 
-  // Click on every "load to card" button.
-  var load2crd = document.getElementsByClassName('fl-coupon-load');
-  console.log(load2crd.length + ' coupons found');
-  var clicked = 0;
+  // Click on "Clip Coupon" buttons.
+  let btns = findButtons('Clip Coupon');
+  console.log(btns.length + ' coupons found');
 
-  // Iterate in reverse because clicking on a button mutates the coupon list.
-  for (var btn of Array.from(load2crd).reverse()) {
+  let clicked = 0;
+  for (let btn of Array.from(btns).reverse()) {
     btn.scrollIntoView();
     btn.click();
     clicked++;
     await sleep(1000);
   }
+
   console.log(clicked + ' coupons clicked');
 }
 
 function insertButton(btn) {
   function waitForSite() {
-    var targetelem = document.getElementById('site-content-wrap');
+    let targetelem = document.getElementById('app');
     if (targetelem !== null) {
       clearInterval(waitForSiteTimer);
       targetelem.insertBefore(btn, targetelem.childNodes[0]);
@@ -38,12 +52,12 @@ function insertButton(btn) {
   }
 
   // Wait for site to finish loading before inserting button.
-  var waitForSiteTimer = setInterval(waitForSite, 100);
+  let waitForSiteTimer = setInterval(waitForSite, 100);
 }
 
 function init() {
   // Make a new button for our action.
-  var newbutton = document.createElement('button');
+  let newbutton = document.createElement('button');
   newbutton.name = 'load_all_to_card';
   newbutton.id = 'load_all_to_card';
   newbutton.style.cssText = 'background-color: #fff; color: #E82A24; font-weight: 700; border: solid #E82A24; padding: 6px 10px; cursor: pointer; margin: 5px';
